ee2ff113e5604343b0e89bdffa5201fbdbb063b3
Tshiamo Todd <tshiamo.mokwena19@gmail.com>
2024-07-09 12:15:29 +0200

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY?.toString(),
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN?.toString(),
-    projectId: process.env.FIREBASE_PROJECT_ID?.toString(),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET?.toString(),
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID?.toString(),
-    appId: process.env.FIREBASE_APP_ID?.toString()
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
