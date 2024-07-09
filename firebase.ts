6641da10624fd7222cd0f240e8a13af35ae89b20
Tshiamo Todd <tshiamo.mokwena19@gmail.com>
2024-07-09 12:00:24 +0200

    Stringify environment variables in firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: process.env.FIREBASE_API_KEY?.toString(),
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN?.toString(),
+    projectId: process.env.FIREBASE_PROJECT_ID?.toString(),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET?.toString(),
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID?.toString(),
+    appId: process.env.FIREBASE_APP_ID?.toString()
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
